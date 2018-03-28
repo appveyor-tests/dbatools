03b6fd526f9c1137dab63e3eeea21385c9fee743
Feodor Fitsner <feodor@appveyor.com>
2018-03-28 13:52:04 -0700

    Update appveyor.sqlserver.ps1

--- tests/appveyor.sqlserver.ps1
+++ tests/appveyor.sqlserver.ps1
@@ -3,14 +3,11 @@ New-Item -Path C:\temp -ItemType Directory -ErrorAction SilentlyContinue | Out-N
 New-Item -Path C:\temp\migration -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 New-Item -Path C:\temp\backups -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 
-
-if ($env:SCENARIO) {
-    Write-Host -Object "Scenario $($env:scenario)" -ForegroundColor DarkGreen
-    #Write-Host -Object "Main instance $($env:MAIN_INSTANCE)" -ForegroundColor DarkGreen
-    #Write-Host -Object "Setup scripts $($env:SETUP_SCRIPTS)" -ForegroundColor DarkGreen
-    $Setup_Scripts = $env:SETUP_SCRIPTS.split(',').Trim()
-    foreach ($Setup_Script in $Setup_Scripts) {
-        $SetupScriptPath = Join-Path $env:APPVEYOR_BUILD_FOLDER $Setup_Script
-        . $SetupScriptPath
-    }
+Write-Host -Object "Scenario $($env:scenario)" -ForegroundColor DarkGreen
+#Write-Host -Object "Main instance $($env:MAIN_INSTANCE)" -ForegroundColor DarkGreen
+#Write-Host -Object "Setup scripts $($env:SETUP_SCRIPTS)" -ForegroundColor DarkGreen
+$Setup_Scripts = $env:SETUP_SCRIPTS.split(',').Trim()
+foreach ($Setup_Script in $Setup_Scripts) {
+    $SetupScriptPath = Join-Path $env:APPVEYOR_BUILD_FOLDER $Setup_Script
+    . $SetupScriptPath
 }
